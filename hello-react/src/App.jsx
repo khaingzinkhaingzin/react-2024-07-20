0c40a707d8207d554e7f27ecaf56a0b6b346c791
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-07-21 15:25:40 +0630

    Form

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import List from "./List.jsx";
 import Item from "./Item.jsx";
+import Form from "./Form";
 
 export default function App() {
   const [data, setData] = useState([
@@ -10,15 +11,25 @@ export default function App() {
     { id: 3, content: "Yay, interesting.", name: "Chris" },
   ]);
 
+  const remove = id => {
+    setData(data.filter(item => item.id !== id));
+  }
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, {id, content, name}]);
+  }
+
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Hello React</h1>
+      <Form add={add} />
       <List>
         {data.map(item => {
           return <Item 
             key={item.id}
-            content={item.content} 
-            name={item.name} />
+            item={item} 
+            remove={remove} />
         })}
       </List>
     </div>
