f0501dc4307a2b76c4e2f2e164cdff996408661b
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-07-22 21:26:02 +0630

    conditional rendering

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -11,6 +11,8 @@ export default function App() {
     { id: 3, content: "Yay, interesting.", name: "Chris" },
   ]);
 
+  const [showForm, setShowForm] = useState(false);
+
   const remove = id => {
     setData(data.filter(item => item.id !== id));
   }
@@ -22,8 +24,26 @@ export default function App() {
 
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
-      <h1>Hello React</h1>
-      <Form add={add} />
+      <h1 style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        Hello React
+        <button
+          onClick={() => setShowForm(!showForm)}
+          style={{
+            width: 32,
+            height: 32,
+            borderRadius: 50,
+            border: "0 none",
+            background: showForm ? "#dc3545" : "#0d6efd",
+            color: "white",
+          }}>
+          {showForm ? "x" : "+"}
+        </button>
+      </h1>
+      {showForm && <Form add={add} />}
       <List>
         {data.map(item => {
           return <Item 
